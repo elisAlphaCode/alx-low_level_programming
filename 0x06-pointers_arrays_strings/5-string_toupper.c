51d3cffa3c16812602ad5e81ccf9cac7cabe23d9
elisAlphaCode <99659934+elisAlphaCode@users.noreply.github.com>
2022-03-24 17:12:02 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,11 @@
 #include "main.h"
  /**
- *  string_toupper - a function changes all lowercase letters of a
+ * string_toupper - a function changes all lowercase letters of a
  * string to uppercase.
  * @s: An input string
  * Return: a char pointer to the converted string
  */
+
 char *string_toupper(char *s)
 {
 		char *start = s;
