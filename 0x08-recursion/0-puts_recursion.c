4b57cf5a6ab443c6b0b5b9446385bfa0d68b30a6
elisAlphaCode <99659934+elisAlphaCode@users.noreply.github.com>
2022-03-30 15:10:36 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_rescursion - prints a string, followed by a new line
- * @s: pointer to the string
- * Return: void
- */
+  * _puts_recursion - prints a string, followed by a new line.
+  * @s: string
+  * Return: pointer
+  */
 
-void _puts_rescursion(char *s)
+void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if  (*s != '\0')
 	{
 		_putchar(*(s + 0));
-		_puts_rescursion((s + 1));
+		_puts_recursion((s + 1));
 	}
 	else
 	{
