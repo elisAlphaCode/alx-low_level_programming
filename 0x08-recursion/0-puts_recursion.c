5154deba5ab30fc4e2fe3b6e03fe9d5cf9057bc7
elisAlphaCode <elis.joshua55@gmail.com>
2022-03-30 14:49:17 +0100

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_rescursion - prints a string, followed by a new line
+ * @s: pointer to the string
+ * Return: void
+ */
+
+void _puts_rescursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_rescursion(s + 1);
+	}
+}
