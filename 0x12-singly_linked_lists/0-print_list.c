9e5122f8c19aa9040646627a536eb23c2acdcff0
elisAlphaCode <elis.joshua55@gmail.com>
2022-04-22 19:35:03 +0100

    prints all the elements of a linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t index = 0;
+	size_t s = 0;
 
 	while (h)
 	{
@@ -19,7 +19,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		index++;
+		s++;
 	}
-	return (index);
+	return (s);
 }
