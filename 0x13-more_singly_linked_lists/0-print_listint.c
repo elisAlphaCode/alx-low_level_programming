96d147814f2e2409b95fbb09373ccd2b609d5832
elisAlphaCode <elis.joshua55@gmail.com>
2022-04-27 03:35:05 +0100

    print all the elements of a linkedlists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,17 +1,15 @@
-#include "list.h"
+#include "lists.h"
 #include <stdio.h>
 
 /**
- * print_lsitint - print elements of linkedlists
+ * print_lsitint - print all the elements of a linkedlists
  * @h: head
  * Return: length of the string
- * 
  */
 
 size_t print_lsitint(const listint_t *h)
 {
 	size_t index_nodes = 0;
-	
 	const listint_t *currentv = h;
 
 	while (currentv != NULL)
